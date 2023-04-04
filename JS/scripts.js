7e6caef872971479dde630d65b61c08725803104
Emma Gerigscott <elgerig@gmail.com>
2023-04-04 11:01:58 -0700

    check for empty deposit or withdraw inputs before running methods
    Co-authored-by: Erik Zambrano <molagg92@gmail.com>

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,5 @@
+// Buisness Logic ---------------
+
 function Bank() {
     this.accounts = {};
     this.currentId = 0;
@@ -13,12 +15,46 @@ Bank.prototype.assignId = function() {
     return this.currentId;
 }
 
-function BankAccount(money) {
-    this.checking = money;
+function BankAccount(name, money) {
+    this.name = name;
+    this.checking = parseInt(money);
 }
 
 BankAccount.prototype.withdraw = function(withdrawal) {
-    this.checking -= withdrawal;
+    this.checking -= parseInt(withdrawal);
+    return this.checking;
+}
+BankAccount.prototype.deposit = function(amount) {
+    this.checking += parseInt(amount);
     return this.checking;
 }
 
+
+
+//Interface Logic -------------
+let bigBank = new Bank();
+
+function handleAccountSubmit(e) {
+    e.preventDefault();
+    const inputtedName = document.getElementById("name").value;
+    const inputtedFunds = parseInt(document.getElementById("money").value);
+    let newAccount = new BankAccount(inputtedName, inputtedFunds);
+    bigBank.addAccount(newAccount);
+}
+
+function handleDepositAndWithraw(e) {
+    e.preventDefault();
+    const inputtedDeposit = document.getElementById("deposit").value;
+    const inputtedWithdraw = document.getElementById("withdraw").value;
+    if (inputtedDeposit !== "") {
+        bigBank.accounts["1"].deposit(inputtedDeposit);
+    }
+    if (inputtedWithdraw !== "") {
+        bigBank.accounts["1"].withdraw(inputtedWithdraw);
+    }
+}
+
+window.addEventListener("load", function() {
+    document.querySelector("form#initiate-account").addEventListener("submit", handleAccountSubmit);
+    document.querySelector("form#deposit-withdraw").addEventListener("submit", handleDepositAndWithraw)
+});
