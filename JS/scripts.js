ab4003d255cc0c66bbbf45f3b79c109632ebcb71
Emma Gerigscott <elgerig@gmail.com>
2023-04-04 14:18:00 -0700

    user search functionality, look up account number and display account info

--- JS/scripts.js
+++ JS/scripts.js
@@ -14,6 +14,12 @@ Bank.prototype.assignId = function() {
     this.currentId += 1
     return this.currentId;
 }
+Bank.prototype.search = function(id) {
+    if (this.accounts[id] !== undefined ) {
+        return this.accounts[id];
+    } 
+    return false;
+}
 
 function BankAccount(name, money) {
     this.name = name;
@@ -35,37 +41,49 @@ BankAccount.prototype.deposit = function(amount) {
 let bigBank = new Bank();
 
 function handleAccountSubmit(e) {
-    e.preventDefault();
-    const inputtedName = document.getElementById("name").value;
-    const inputtedFunds = parseInt(document.getElementById("money").value);
-    let newAccount = new BankAccount(inputtedName, inputtedFunds);
-    bigBank.addAccount(newAccount);
-    displayCurrentBalance()
-    document.getElementById("name").value = null;
-    document.getElementById("money").value = null;
+	e.preventDefault();
+	const inputtedName = document.getElementById("name").value;
+	const inputtedFunds = parseInt(document.getElementById("money").value);
+	let newAccount = new BankAccount(inputtedName, inputtedFunds);
+	bigBank.addAccount(newAccount);
+	displayCurrentBalance()
+	document.getElementById("name").value = null;
+	document.getElementById("money").value = null;
 
-    document.getElementById("initiate-account").setAttribute("class", "hidden");
-    document.getElementById("current-balance").removeAttribute("class")
+	document.getElementById("initiate-account").setAttribute("class", "hidden");
+	document.getElementById("current-balance").removeAttribute("class")
 }
 
 function handleDepositAndWithraw(e) {
-    e.preventDefault();
-    const inputtedDeposit = document.getElementById("deposit").value;
-    const inputtedWithdraw = document.getElementById("withdraw").value;
-    if (inputtedDeposit !== "") {
-        bigBank.accounts["1"].deposit(inputtedDeposit);
-    }
-    if (inputtedWithdraw !== "") {
-        bigBank.accounts["1"].withdraw(inputtedWithdraw);
-    }
-    displayCurrentBalance()
-    document.getElementById("deposit").value = null;
-    document.getElementById("withdraw").value = null;
+	e.preventDefault();
+	const inputtedDeposit = document.getElementById("deposit").value;
+	const inputtedWithdraw = document.getElementById("withdraw").value;
+	if (inputtedDeposit !== "") {
+			bigBank.accounts["1"].deposit(inputtedDeposit);
+	}
+	if (inputtedWithdraw !== "") {
+			bigBank.accounts["1"].withdraw(inputtedWithdraw);
+	}
+	displayCurrentBalance()
+	document.getElementById("deposit").value = null;
+	document.getElementById("withdraw").value = null;
+}
+
+function handleSearchSubmit(e) {
+	e.preventDefault();
+	const inputtedSearch = document.getElementById("searchInput").value;
+	const searchResult = bigBank.search(inputtedSearch);
+	displaySearchResult(searchResult);
+	document.getElementById("searchResult").removeAttribute("class");
+	document.getElementById("current-balance").setAttribute("class", "hidden");
+	document.getElementById("initiate-account").removeAttribute("class");
+	document.getElementById("searchInput").value = null;
 }
 
 function showAccountCreater() {
     document.getElementById("initiate-account").removeAttribute("class");
     document.getElementById("current-balance").setAttribute("class", "hidden")
+    
 }
 
 function displayCurrentBalance() {
@@ -73,10 +91,18 @@ function displayCurrentBalance() {
     document.getElementById("amount").innerText = "$" + bigBank.accounts[account].checking;
     document.getElementById("accountNumber").innerText = bigBank.accounts[account].id;
     document.getElementById("accountHolder").innerText = bigBank.accounts[account].name;
+		document.getElementById("searchResult").setAttribute("class", "hidden")
+}
+
+function displaySearchResult(account) {
+	document.getElementById("search-amount").innerText = "$" + account.checking;
+	document.getElementById("search-accountNumber").innerText = account.id;
+	document.getElementById("search-accountHolder").innerText = account.name;
 }
 
 window.addEventListener("load", function() {
-    document.querySelector("form#initiate-account").addEventListener("submit", handleAccountSubmit);
-    document.querySelector("form#deposit-withdraw").addEventListener("submit", handleDepositAndWithraw)
-    document.querySelector("button#accountMaker").addEventListener("click", showAccountCreater)
+	document.querySelector("form#search").addEventListener("submit", handleSearchSubmit);
+	document.querySelector("form#initiate-account").addEventListener("submit", handleAccountSubmit);
+	document.querySelector("form#deposit-withdraw").addEventListener("submit", handleDepositAndWithraw)
+	document.querySelector("button#accountMaker").addEventListener("click", showAccountCreater)
 });
